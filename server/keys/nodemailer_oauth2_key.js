fce1ba4c7016cc0ee936b9e11811b5f02f25b792
Ahmed <46319030+AhmedAlsaab@users.noreply.github.com>
2019-05-23 21:40:51 +0100

    Removed Nodemailer keys

--- server/keys/nodemailer_oauth2_key.js
+++ server/keys/nodemailer_oauth2_key.js
@@ -1,6 +1,6 @@
-const clientId = '768645817883-ro9hm565vjqinro0hjqg5g0nfgiobd9v.apps.googleusercontent.com';
-const clientSecret = 'zVG9lDBfOz0Bb8Ph-GFF_upk';
-const refreshToken = '1/B4Q0UUOQzelsVLd1lT3y-CABcowYT7HVLtgJJmH2AKM';
+const clientId = 'YOUR NODEMAIL_OATH2 CLIENT ID';
+const clientSecret = 'YOUR NODEMAIL_OATH2 CLIENT SECRET';
+const refreshToken = 'YOUR NODEMAIL_OATH2 REFRESH TOKEN';
 
 exports.clientId = clientId;
 exports.clientSecret = clientSecret;
