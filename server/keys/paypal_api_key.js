6bcef2a733a4e1f9a99fed3b120b4dbef1001f56
Ahmed <46319030+AhmedAlsaab@users.noreply.github.com>
2019-05-23 21:39:28 +0100

    Removed Paypal API key

--- server/keys/paypal_api_key.js
+++ server/keys/paypal_api_key.js
@@ -1,5 +1,5 @@
-const id = 'Afy658lwPN5B8k8kKcDRII16BcQ7iG78rD3nJswnZvI45_jXxD3eQYllGnoqHuaCWnaJFGX_Xe0xgTDS'
-const secret = 'ENVPVfbFU5yaxHxTb1ocVq6-e8CvcPUvppG_lV8f5LOACiyGcRzjvDCRarJa3ZB6QNei5iIWiQyqBozM'
+const id = 'YOUR PAYPAL SANDBOX ID'
+const secret = 'YOUR PAYPAL SANDBOX SECRET'
 
 exports.client_id = id;
 exports.client_secret = secret;
